127897cc3efb5aeb15832337bb94d64d50dfef69
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-09 15:46:44 +0800

    add view page

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Document</title>
-    <style>
-        header { 
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1><?=
-                'HELLO, '. htmlspecialchars($_GET['name']);    
-            ?>
-        </h1>
-    </header>
-</body>
-</html> 
+<?php
+
+    $greeting = "HELLO";
+    require 'index.view.php';

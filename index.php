2228b6740e8003c0eaaddc8bd7f5d3f35fdd0717
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-09 14:34:55 +0800

    init only html

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Document</title>
+    <style>
+        header { 
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>hello</h1>
+    </header>
+</body>
+</html> 

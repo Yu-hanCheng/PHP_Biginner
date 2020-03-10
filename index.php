3c57c050ab70352db3cc25e86afdc915b0293abf
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-10 15:08:27 +0800

    PDO::FETCH_CLASS

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+    require 'Task.php';
+
     try {
         $pdo = new PDO('mysql:host=127.0.0.1;dbname=todolist','sarahcheng','www^0^Yoyo409');
     } catch (PDOException $e) {
@@ -9,6 +11,6 @@
     $statement = $pdo->prepare('select * from todos');
     $statement->execute();
 
-    $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
+    $tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
     var_dump($tasks);
     require 'index.view.php';

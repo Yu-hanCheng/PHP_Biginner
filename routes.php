8905fab1f8883addc473fd65106f8a097e17e219
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-29 21:26:22 +0800

    [FEAT] form GET/ parse_url

--- routes.php
+++ routes.php
@@ -3,5 +3,6 @@
     $router->define([
         '' => 'controllers/index.php',
         'about' => 'controllers/about.php',
-        'about-contact' => 'controllers/about-contact.php'
+        'about-contact' => 'controllers/about-contact.php',
+        'names' => 'controllers/add-name.php'
     ]);
--- views/index.view.php
+++ views/index.view.php
@@ -1,16 +1,21 @@
 <?php require('partials/head.php') ?>
 
-    <ul>
-        <?php foreach ($tasks as $task) : ?>
-        <li>
-            <?php if ($task->completed) : ?>
-            <strike><?= $task->description; ?></strike>
-            <span class="icon">&#9989;</span>
-            <?php else: ?>
-            <?= $task->description; ?>
-            <?php endif; ?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+<form action="/names" method="get">
+    <input name="name"></input>
+    <button type="submit">submit</button>
+</form>
+
+<ul>
+    <?php foreach ($tasks as $task) : ?>
+    <li>
+        <?php if ($task->completed) : ?>
+        <strike><?= $task->description; ?></strike>
+        <span class="icon">&#9989;</span>
+        <?php else: ?>
+        <?= $task->description; ?>
+        <?php endif; ?>
+    </li>
+    <?php endforeach; ?>
+</ul>
 
 <?php require('partials/footer.php') ?>

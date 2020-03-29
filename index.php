b5de5d1943807633081d3fd72e01e01cb3eb4872
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-29 18:29:16 +0800

    [REFACTOR] add class Request

--- index.php
+++ index.php
@@ -4,7 +4,4 @@ $database = require 'core/bootstrap.php';
 
 $router = new Router;
 
-
-$uri = trim($_SERVER['REQUEST_URI'],'/');
-
-require Router::load('routes.php')->direct($uri);
+require Router::load('routes.php')->direct(Request::uri());

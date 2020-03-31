3b9b217875d8a4cd777746a97fb740e3b9bfeebf
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-31 16:55:08 +0800

    [FEREFACTOR] c view function in bootstrap

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -6,17 +6,17 @@ class PageController
     {
         $tasks = App::get('database')->selectAll('todos');
         $users = App::get('database')->selectAll('user_test');
-        require 'views/index.view.php';
+        return view('index', ['tasks' => $tasks, 'users' => $users]);
     }
 
     public function about()
     {
-        require 'views/about.view.php';
+        return view('about');
     }
 
     public function contact()
     {
         $name = "laracast";
-        require 'views/contact.view.php';
+        return view('contact');
     }
 }
